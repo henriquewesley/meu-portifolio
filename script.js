e93129cdc5b773b1e865198d687381e8e5c8b52c
henriquewesley <whenfeof@gmail.com>
2025-03-28 07:07:08 -0300

    melhorias na responsividade

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 const linkHome = document.querySelector("#ir-home");
-const linkprojetos = document.querySelector("#ir-projetos");
-const linksobre = document.querySelector("#ir-sobre");
-const linkcontatos = document.querySelector("#ir-contatos");
+const linkProjetos = document.querySelector("#ir-projetos");
+const linkSobre = document.querySelector("#ir-sobre");
+const linkContatos = document.querySelector("#ir-contatos");
 
 function cliqueiHome() {
-  console.log("clicou em home");
+  console.log(linkHome);
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
 }
 
-select.addEventListener("click", cliqueiHome);
+linkHome.addEventListener("click", cliqueiHome);
 
 function cliqueiProjetos() {
-  console.log(linkprojetos);
+  console.log(linkProjetos);
   window.scrollTo({
     top: 600,
     behavior: "smooth",
   });
 }
 
-linkprojetos.addEventListener("click", cliqueiProjetos);
+linkProjetos.addEventListener("click", cliqueiProjetos);
 
 function cliqueiSobre() {
-  console.log("clicou em sobre");
+  console.log(linkSobre);
   window.scrollTo({
     top: 1300,
     behavior: "smooth",
   });
 }
 
-select.addEventListener("click", cliqueiSobre);
+linkSobre.addEventListener("click", cliqueiSobre);
 
 function cliqueiContatos() {
-  console.log("clicou em contatos");
+  console.log(linkContatos);
   window.scrollTo({
     top: 2200,
     behavior: "smooth",
   });
 }
 
-select.addEventListener("click", cliqueiContatos);
+linkContatos.addEventListener("click", cliqueiContatos);
